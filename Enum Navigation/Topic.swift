b7718642d54e131c4ecc49e46fb9e9adaddd1dd6
David Grammatico <david.lugdunum@gmail.com>
2023-10-30 22:55:57 +0100

    implement enum view

---
+++ Enum Navigation/Topic.swift
@@ -0,0 +1,33 @@
+//
+//  Topic.swift
+//  Enum Navigation
+//
+//  Created by David Grammatico on 30/10/2023.
+//
+
+import SwiftUI
+
+enum Topic: String, Hashable, CaseIterable, Identifiable, View {
+    case topic1 = "Green Room"
+    case topic2 = "Stewart Lynch"
+    case topic3 = "Rainbow"
+    case topic4 = "Inukshuk"
+    
+    var id: String {
+        self.rawValue
+    }
+    
+    var body: some View {
+        switch self {
+        case .topic1:
+            Topic1View(title: self.rawValue)
+        case .topic2:
+            Topic2View(title: self.rawValue)
+        case .topic3:
+            Topic3View(title: self.rawValue, colors: [.red, .orange, .yellow, .blue, .indigo, .purple])
+        case .topic4:
+            Topic4View(title: self.rawValue, imageName: "Inukshuk")
+        }
+    }
+    
+}
